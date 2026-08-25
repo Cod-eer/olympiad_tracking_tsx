068d15e652bd7c536d1474fc0c2fde4d1e26d7ec
Timofei Kovalev <timkovalev12@gmail.com>
2026-08-25 23:01:30 +0300

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: [
-    'playwright-core'
-  ],
+  serverExternalPackages: ["playwright-core"],
+  outputFileTracingIncludes: {
+    // Tell Vercel to include browsers.json whenever tracing serverless functions under app/ backend
+    "/**/*": ["./node_modules/playwright-core/browsers.json"],
+  },
 };
 
 export default nextConfig;
